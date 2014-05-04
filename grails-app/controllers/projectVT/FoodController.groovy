66ecd3531e4c606d988254160ece49363f191459
jay <jack08300@hotmail.com>
2014-05-04 18:44:58 -0400

    fix vitamin require calculation bug

--- grails-app/controllers/projectVT/FoodController.groovy
+++ grails-app/controllers/projectVT/FoodController.groovy
@@ -38,8 +38,19 @@ class FoodController {
         println foodList;
         println gender;
         def foodAndAmountArray = foodList.split(",")
-        Map resultMap = [a:0,b1:0,b2:0,b3:0,b6:0,b12:0,c:0,d:0,e:0,k:0]
+
         def vitaminRequire = DailyAmount.findByGender(gender)
+        Map resultMap = [a:vitaminRequire.vitaminA,
+                b1:vitaminRequire.vitaminB1,
+                b2:vitaminRequire.vitaminB2,
+                b3:vitaminRequire.vitaminB3,
+                b6:vitaminRequire.vitaminB6,
+                b12:vitaminRequire.vitaminB12,
+                c:vitaminRequire.vitaminC,
+                d:vitaminRequire.vitaminD,
+                e:vitaminRequire.vitaminE,
+                k:vitaminRequire.vitaminK];
+
         foodAndAmountArray.each(){ foodAndAmount->
             //Array 0 -> food name, 1 -> amount
             def array = foodAndAmount.split(":")
@@ -47,16 +58,16 @@ class FoodController {
             def vitaminData = Vitamin.findByFood(Food.findByName(array[0]))
             if(vitaminData){
                 double amount = vitaminData.gram*Double.parseDouble(array[1]);
-                resultMap = [a: resultMap.a + vitaminRequire.vitaminA - (vitaminData.vitaminA*amount),
-                        b1: resultMap.b1 + vitaminRequire.vitaminB1 - (vitaminData.vitaminB1*amount),
-                        b2: resultMap.b2 + vitaminRequire.vitaminB2 - (vitaminData.vitaminB2*amount),
-                        b3: resultMap.b3 + vitaminRequire.vitaminB3 - (vitaminData.vitaminB3*amount),
-                        b6: resultMap.b6 + vitaminRequire.vitaminB6 - (vitaminData.vitaminB6*amount),
-                        b12: resultMap.b12 + vitaminRequire.vitaminB12 - (vitaminData.vitaminB12*amount),
-                        c: resultMap.c + vitaminRequire.vitaminC - (vitaminData.vitaminC*amount),
-                        d: resultMap.d + vitaminRequire.vitaminD - (vitaminData.vitaminD*amount),
-                        e: resultMap.e + vitaminRequire.vitaminE - (vitaminData.vitaminE*amount),
-                        k: resultMap.k + vitaminRequire.vitaminK - (vitaminData.vitaminK*amount)]
+                resultMap = [a: resultMap.a - (vitaminData.vitaminA*amount),
+                        b1: resultMap.b1 - (vitaminData.vitaminB1*amount),
+                        b2: resultMap.b2 - (vitaminData.vitaminB2*amount),
+                        b3: resultMap.b3 - (vitaminData.vitaminB3*amount),
+                        b6: resultMap.b6 - (vitaminData.vitaminB6*amount),
+                        b12: resultMap.b12  - (vitaminData.vitaminB12*amount),
+                        c: resultMap.c - (vitaminData.vitaminC*amount),
+                        d: resultMap.d - (vitaminData.vitaminD*amount),
+                        e: resultMap.e - (vitaminData.vitaminE*amount),
+                        k: resultMap.k - (vitaminData.vitaminK*amount)]
             }
         }
 
