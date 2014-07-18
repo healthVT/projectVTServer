b53d3c10cc7cbc1d39f37f40c0c02093eccebbca
jay <jack08300@hotmail.com>
2014-07-17 22:23:23 -0400

    fix daily vitamin calculation

--- grails-app/controllers/projectVT/FoodController.groovy
+++ grails-app/controllers/projectVT/FoodController.groovy
@@ -2,6 +2,8 @@ package projectVT
 
 import grails.converters.JSON
 
+import java.text.DecimalFormat
+
 
 class FoodController {
 
@@ -35,12 +37,21 @@ class FoodController {
     }
 
     def getVitaminDailyResult(String foodList, String gender, int age){
-        println foodList;
-        println gender;
         def foodAndAmountArray = foodList.split(",")
 
+        int vitaminA = 0
+        int vitaminB1 = 0
+        int vitaminB2 = 0
+        int vitaminB3 = 0
+        int vitaminB6 = 0
+        int vitaminB12 = 0
+        int vitaminC = 0
+        int vitaminD = 0
+        int vitaminE = 0
+        int vitaminK = 0
+
         def vitaminRequire = DailyAmount.findByGender(gender)
-        Map resultMap = [a:vitaminRequire.vitaminA,
+        Map requireMap = [a:vitaminRequire.vitaminA,
                 b1:vitaminRequire.vitaminB1,
                 b2:vitaminRequire.vitaminB2,
                 b3:vitaminRequire.vitaminB3,
@@ -49,28 +60,44 @@ class FoodController {
                 c:vitaminRequire.vitaminC,
                 d:vitaminRequire.vitaminD,
                 e:vitaminRequire.vitaminE,
-                k:vitaminRequire.vitaminK];
+                k:vitaminRequire.vitaminK]
 
-        foodAndAmountArray.each(){ foodAndAmount->
+        foodAndAmountArray.each(){
             //Array 0 -> food name, 1 -> amount
-            def array = foodAndAmount.split(":")
-
+            def array = it.split(":")
             def vitaminData = Vitamin.findByFood(Food.findByName(array[0]))
+
             if(vitaminData){
-                double amount = vitaminData.gram*Double.parseDouble(array[1]);
-                resultMap = [a: resultMap.a - (vitaminData.vitaminA*amount),
-                        b1: resultMap.b1 - (vitaminData.vitaminB1*amount),
-                        b2: resultMap.b2 - (vitaminData.vitaminB2*amount),
-                        b3: resultMap.b3 - (vitaminData.vitaminB3*amount),
-                        b6: resultMap.b6 - (vitaminData.vitaminB6*amount),
-                        b12: resultMap.b12  - (vitaminData.vitaminB12*amount),
-                        c: resultMap.c - (vitaminData.vitaminC*amount),
-                        d: resultMap.d - (vitaminData.vitaminD*amount),
-                        e: resultMap.e - (vitaminData.vitaminE*amount),
-                        k: resultMap.k - (vitaminData.vitaminK*amount)]
+                double amount = Double.parseDouble(array[1]);
+                vitaminA += vitaminData.vitaminA*amount
+                vitaminB1 += vitaminData.vitaminB1*amount
+                vitaminB2 += vitaminData.vitaminB2*amount
+                vitaminB3 += vitaminData.vitaminB3*amount
+                vitaminB6 += vitaminData.vitaminB6*amount
+                vitaminB12 += vitaminData.vitaminB12*amount
+                vitaminC += vitaminData.vitaminC*amount
+                vitaminD += vitaminData.vitaminD*amount
+                vitaminE += vitaminData.vitaminE*amount
+                vitaminK += vitaminData.vitaminK*amount
             }
+
+
         }
 
+        DecimalFormat df = new DecimalFormat("###");
+        def resultMap = [
+                a:  df.format(vitaminA/requireMap.a*100),
+                b1: df.format(vitaminB1/requireMap.b1*100),
+                b2: df.format(vitaminB2/requireMap.b2*100),
+                b3: df.format(vitaminB3/requireMap.b3*100),
+                b6: df.format(vitaminB6/requireMap.b6*100),
+                b12:df.format(vitaminB12/requireMap.b12*100),
+                c:  df.format(vitaminC/requireMap.c*100),
+                d:  df.format(vitaminD/requireMap.d*100),
+                e:  df.format(vitaminE/requireMap.e*100),
+                k:  df.format(vitaminK/requireMap.k*100)]
+
+        
         render resultMap as JSON
     }
 }
